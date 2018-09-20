66468fcbe4585b28b72bf07dafda49a17a936bf3
Simon Harrer <simon.harrer@innoq.com>
2018-09-20 19:08:35 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,25 @@
+# Tool for Remote Mob Programming
+
+Allows to switch the typist in a remote mob programming session quickly. Syncs via a "mob feature branch".
+
+## How to use it?
+
+```bash
+# simon starts as first typist
+simon$ mob start
+# after x minutes simon ends his mobbing interval
+simon$ mob next
+# carola takes over as the second typist
+carola$ mob start
+# after x minutes carola ends her mobbing interval
+carola$ mob next
+# our feature is already done
+carola$ mob done
+carola$ git commit --message "describe what the mob session was all about"
+```
+
+## How does it work?
+
+- `mob start` creates branch `mob-session` and pulls from `origin/mob-session`
+- `mob next` pushes all changes to `origin/mob-session`in a `WIP [ci-skip]` commit
+- `mob done` squashes all changes in `mob-session` into staging of `master` and removes `mob-session` and `origin/mob-session` 
---
+++ mob.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+const branch = "mob-session"
+const debug = true
+
+// -d to enable insights into what happened in git
+
+func main() {
+	argument := getCommand()
+	if argument == "s" || argument == "start" {
+		start()
+	} else if argument == "n" || argument == "next" {
+		next()
+	} else if argument == "d" || argument == "done" {
+		done()
+	} else if argument == "r" || argument == "reset" {
+		reset()
+	} else if argument == "h" || argument == "help" {
+		help()
+	} else {
+		status()
+	}
+}
+
+func reset() {
+	git("checkout", "master")
+	git("branch", "-D", branch)
+	git("push", "origin", "--delete", branch)
+}
+
+func start() {
+	git("checkout", "-b", branch)
+	git("pull", "--force")
+	say("start hacking")
+}
+
+func next() {
+	git("add", ".", "--all")
+	git("commit", "--message", "\"WIP in Mob Session [ci-skip]\"")
+	git("push")
+	say("join the 'rest of the mob'")
+}
+
+func done() {
+	git("checkout", "master")
+	git("merge", "--squash", branch)
+	git("branch", "-D", branch)
+	git("push", "origin", "--delete", branch)
+	say("lean back, you survived your mob session :-)")
+	say("execute 'git commit' to describe what the mob achieved")
+}
+
+func status() {
+	if isMobbing() {
+		say("mobbing in progress")
+	} else {
+		say("you aren't mobbing right now")
+	}
+}
+
+func isMobbing() bool {
+	output := git("branch")
+	isMobbing := strings.Contains(output, "* "+branch)
+	return isMobbing
+}
+
+func help() {
+	say("usage")
+	say("\tmob [s]tart \t# start mobbing as typist")
+	say("\tmob [n]ext \t# hand over to next typist")
+	say("\tmob [d]one \t# finish mob session")
+	say("\tmob [r]eset \t# resets any unfinished mob session")
+	say("\tmob [h]elp \t# prints this help")
+}
+
+func git(args ...string) string {
+	command := exec.Command("git", args...)
+	if (debug) {
+		fmt.Println(command.Args)
+	}
+	outputBinary, err := command.CombinedOutput()
+	output := string(outputBinary)
+	if (debug) {
+		fmt.Println(output)
+	}
+	if err != nil {
+		fmt.Println(err)
+	}
+	return output
+}
+
+func say(s string) {
+	fmt.Println(s)
+}
+
+func getCommand() string {
+	args := os.Args
+	if len(args) <= 1 {
+		return ""
+	}
+	return args[1]
+}
