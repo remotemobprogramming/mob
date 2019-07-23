a49a93f0e90214a70d541ec7d40aad1a9e9ecdd7
Simon Harrer <simon.harrer@innoq.com>
2019-07-23 13:36:00 +0200

    Show notifications

--- mob.go
+++ mob.go
@@ -57,7 +57,7 @@ func startTimer(timerInMinutes string) {
 	timeoutInSeconds := timeoutInMinutes * 60
 	timerInSeconds := strconv.Itoa(timeoutInSeconds)
 
-	command := exec.Command("sh", "-c", "( sleep "+timerInSeconds+" && say \"time's up\" & )")
+	command := exec.Command("sh", "-c", "( sleep "+timerInSeconds+" && say \"time's up\" && (/usr/bin/osascript -e 'display notification \"time is up\"' || /usr/bin/notify-send \"time is up\")  & )")
 	if isDebug() {
 		fmt.Println(command.Args)
 	}
