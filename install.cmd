de80bda06ea521cc7731e815f1d223fd261fc59f
Kevin Deggelmann <kdn@sybit.de>
2019-09-30 15:09:48 +0200

    install mob.exe into user's dir (windows only!)
    add this path to env var PATH

--- install.cmd
+++ install.cmd
@@ -1,9 +1,29 @@
 @echo off
 @setlocal
 
-REM probably there is a better place
-set target=%SystemRoot%
+echo Installing 'mob'...
+
+set target="%USERPROFILE%\.mob"
+
+if not exist %target% (
+	md %target%
+	echo Created %target%
+)
 
 go build mob.go
 copy mob.exe %target%
 echo 'mob.exe' installed to %target%
+
+REM add target to PATH
+for /f "tokens=*" %%a in ('echo "%PATH%" ^| find /C /I %target%') do set INPATH=%%a
+
+setx MOB_HOME %target%
+echo Added '%target%' to PATH
+
+
+if %INPATH% EQU 0 (
+	setx PATH %%MOB_HOME%%;"%PATH%"
+	echo Added '%target%' to PATH
+)
+echo Installed 'mob' successfully
+pause
