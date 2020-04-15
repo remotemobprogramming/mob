59af7c5610275e9e55b36a0c831faa52c3f48e9f
Simon Harrer <simon.harrer@innoq.com>
2020-04-15 11:44:06 +0200

    Simpler install script that works on linux as well

--- install.sh
+++ install.sh
@@ -1,30 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
-pushd /tmp/ > /dev/null
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-system="darwin"
-else
-system="linux"
-fi
+case "$(uname -s)" in
+   Darwin)
+      system="darwin"
+     ;;
+   *)
+      system="linux"
+     ;;
+esac
 
 echo "Installing latest 'mob' release from GitHub"
 url=$(curl -s https://api.github.com/repos/remotemobprogramming/mob/releases/latest \
 | grep "browser_download_url.*mob_.*${system}_amd64\.tar\.gz" \
 | cut -d ":" -f 2,3 \
 | tr -d \")
-echo "$url"
+# echo "$url"
 tarball="${url##*/}"
-curl -sSL $url > $tarball
-
-echo "Extracting $tarball"
-tar -xzf $tarball
+curl -sSL $url | tar xz -C /usr/local/bin/ mob
 
 # echo "Installing 'mob' in '/usr/local/bin'"
-chmod +x mob
-mv mob /usr/local/bin/
-
-popd > /dev/null
+chmod +x /usr/local/bin/mob
 
 location="$(which mob)"
 echo "Mob binary location: $location"
