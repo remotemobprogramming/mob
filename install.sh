2e9af5b267ff946c52ff0f10f13d19b07f04e552
Simon Harrer <simon.harrer@innoq.com>
2020-04-16 13:02:17 +0200

    Simpler install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "Installing latest 'mob' release from GitHub"
 case "$(uname -s)" in
    Darwin)
       system="darwin"
@@ -8,18 +9,14 @@ case "$(uname -s)" in
       system="linux"
      ;;
 esac
-
-echo "Installing latest 'mob' release from GitHub"
 url=$(curl -s https://api.github.com/repos/remotemobprogramming/mob/releases/latest \
 | grep "browser_download_url.*mob_.*${system}_amd64\.tar\.gz" \
 | cut -d ":" -f 2,3 \
 | tr -d \")
 # echo "$url"
 tarball="${url##*/}"
-curl -sSL $url | tar xz -C /usr/local/bin/ mob
 
-# echo "Installing 'mob' in '/usr/local/bin'"
-chmod +x /usr/local/bin/mob
+curl -sSL $url | tar xz -C /usr/local/bin/ mob && chmod +x /usr/local/bin/mob
 
 location="$(which mob)"
 echo "Mob binary location: $location"
