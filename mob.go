07c9bb5a854c5e99fb11e1cbaa843e55b8f1ef57
Simon Harrer <simon.harrer@innoq.com>
2020-04-23 11:55:24 +0200

    v0.0.7

--- mob.go
+++ mob.go
@@ -10,7 +10,7 @@ import (
 	"time"
 )
 
-const versionNumber = "0.0.6"
+const versionNumber = "0.0.7"
 
 var wipBranch = "mob-session"               // override with MOB_WIP_BRANCH environment variable
 var baseBranch = "master"                   // override with MOB_BASE_BRANCH environment variable
