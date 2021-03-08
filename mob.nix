bdd7b712aaf13af2bb8c57774155319784a0620c
Jan Seeger <jan.seeger@thenybble.de>
2021-03-08 10:00:31 +0100

    Rewrote nix expression to reuse more variables.

--- mob.nix
+++ mob.nix
@@ -1,14 +1,17 @@
-mobsh = buildGoPackage rec {
+{buildGoPackage, fetchFromGitHub, lib, ...}:
+buildGoPackage rec {
     pname = "mob.sh";
     version = "1.3.0";
+    owner = "remotemobprogramming";
+    repo = "mob";
     src = fetchFromGitHub {
-      owner = "remotemobprogramming";
-      repo = "mob";
+      owner = owner;
+      repo = repo;
       rev = "v${version}";
       sha256 = "04x6cl2r4ja41cmy82p5apyavmdvak6jsclzf2l7islf0pmsnddv";
     };
 
-    goPackagePath = "github.com/remotemobprogramming/mob/";
+    goPackagePath = "github.com/${owner}/${repo}/";
 
     subPackages = [ "." ];
     
@@ -17,6 +20,4 @@ mobsh = buildGoPackage rec {
       homepage = "https://mob.sh";
       license = lib.licenses.mit;
     };
-
-};
-
+}
