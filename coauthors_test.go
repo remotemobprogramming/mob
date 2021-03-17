f503d9e58e3498a867225bb806fe5c05f2fd0207
Alex Launi <dev@launi.me>
2021-03-16 23:52:07 -0400

    Test adding coauthors in --with

--- coauthors_test.go
+++ coauthors_test.go
@@ -151,9 +151,32 @@ func TestStartDoneCoAuthors(t *testing.T) {
 	done()
 
 	output := run(t, "cat", "/tmp/mob/local/.git/SQUASH_MSG")
-	fmt.Println("output", *output)
 	// don't include the person running `mob done`
 	assertOutputNotContains(t, output, "Co-authored-by: local <local@example.com>")
 	// include everyone else in commit order after removing duplicates
 	assertOutputContains(t, output, "\nCo-authored-by: bob <bob@example.com>\nCo-authored-by: alice <alice@example.com>\nCo-authored-by: localother <localother@example.com>\n")
+}
+
+func TestAppendWithCoauthors(t *testing.T) {
+	setup(t)
+	configuration.Coauthors = CoauthorsMap{
+		"bob":   "bob <bob@example.com>",
+		"alice": "alice <alice@example.com>",
+	}
+
+	setWorkingDir("/tmp/mob/local")
+	start(configuration)
+	createFile(t, "file1.txt", "asdf")
+	next(configuration)
+
+	setWorkingDir("/tmp/mob/localother")
+	start(configuration)
+	createFile(t, "file2.txt", "asdf")
+	done()
+
+	output := run(t, "cat", "/tmp/mob/localother/.git/SQUASH_MSG")
+	// don't include the person running `mob done`
+	assertOutputNotContains(t, output, "Co-authored-by: localother <localother@example.com>")
+	// include everyone else in commit order after removing duplicates
+	assertOutputContains(t, output, "\nCo-authored-by: bob <bob@example.com>\nCo-authored-by: alice <alice@example.com>\nCo-authored-by: local <local@example.com>\n")
 }
