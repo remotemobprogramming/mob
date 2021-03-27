6d5e4e9c018f4e1b71a3373b4913458b54998605
Gregor Riegler <gregor.riegler@ebcont.com>
2021-03-27 14:12:02 +0100

    Can mark wip commits for squashing

--- squash_wip_commits_test.go
+++ squash_wip_commits_test.go
@@ -50,3 +50,75 @@ func endsWithWipCommit(configuration Configuration) bool {
 	lines := strings.Split(strings.TrimSpace(log), "\n")
 	return lines[0] == configuration.WipCommitMessage
 }
+
+func TestMarkSquashWip_singleManualCommit(t *testing.T) {
+	configuration = getDefaultConfiguration()
+	lines := "pick c51a56d new file\n" +
+		"\n" +
+		"# Rebase ...`"
+
+	result := markSquashWip(lines, configuration)
+
+	equals(t, lines, result)
+}
+
+func TestMarkSquashWip_manyManualCommits(t *testing.T) {
+	configuration = getDefaultConfiguration()
+	lines := "pick c51a56d new file\n" +
+		"pick 63ef7a4 another commit\n" +
+		"\n" +
+		"# Rebase ...`"
+
+	result := markSquashWip(lines, configuration)
+
+	equals(t, lines, result)
+}
+
+func TestMarkSquashWip_wipCommitFollowedByManualCommit(t *testing.T) {
+	configuration = getDefaultConfiguration()
+	lines := "pick 01a9a31 " + configuration.WipCommitMessage + "\n" +
+		"pick c51a56d manual commit\n" +
+		"\n" +
+		"# Rebase ..."
+	expected := "squash 01a9a31 " + configuration.WipCommitMessage + "\n" +
+		"pick c51a56d manual commit\n" +
+		"\n" +
+		"# Rebase ..."
+
+	result := markSquashWip(lines, configuration)
+
+	equals(t, expected, result)
+}
+
+func TestMarkSquashWip_manyWipCommitsFollowedByManualCommit(t *testing.T) {
+	configuration = getDefaultConfiguration()
+	lines := "pick 01a9a31 " + configuration.WipCommitMessage + "\n" +
+		"pick 01a9a32 " + configuration.WipCommitMessage + "\n" +
+		"pick 01a9a33 " + configuration.WipCommitMessage + "\n" +
+		"pick c51a56d manual commit\n" +
+		"\n" +
+		"# Rebase ..."
+	expected := "squash 01a9a31 " + configuration.WipCommitMessage + "\n" +
+		"squash 01a9a32 " + configuration.WipCommitMessage + "\n" +
+		"squash 01a9a33 " + configuration.WipCommitMessage + "\n" +
+		"pick c51a56d manual commit\n" +
+		"\n" +
+		"# Rebase ..."
+
+	result := markSquashWip(lines, configuration)
+
+	equals(t, expected, result)
+}
+
+func markSquashWip(content string, configuration Configuration) string {
+	lines := strings.Split(strings.TrimSpace(content), "\n")
+	var result = make([]string, len(lines))
+	for i, line := range lines {
+		if strings.HasPrefix(line, "pick ") && strings.HasSuffix(line, configuration.WipCommitMessage) {
+			result[i] = strings.Replace(line, "pick ", "squash ", 1)
+		} else {
+			result[i] = line
+		}
+	}
+	return strings.Join(result, "\n")
+}
