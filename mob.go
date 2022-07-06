8126da6207078bfb354c74e17d51dfdda320e6db
maciej-stanuch <maciejstanuch.ms@gmail.com>
2022-07-06 17:11:14 +0200

    Allow for use of timer outside of git repository

--- mob.go
+++ mob.go
@@ -951,6 +951,11 @@ func startTimer(timerInMinutes string, configuration Configuration) {
 }
 
 func getMobTimerRoom(configuration Configuration) string {
+	if !isGit() {
+		debugInfo("timer not in git repository, using MOB_TIMER_ROOM for room name")
+		return configuration.TimerRoom
+	}
+
 	currentWipBranchQualifier := configuration.WipBranchQualifier
 	if currentWipBranchQualifier == "" {
 		currentBranch := gitCurrentBranch()
@@ -966,6 +971,7 @@ func getMobTimerRoom(configuration Configuration) string {
 		sayInfo("Using wip branch qualifier for room name")
 		return currentWipBranchQualifier
 	}
+
 	return configuration.TimerRoom
 }
 
