3dc99fec3b9d52cf022fde96ed10545c9a90dd31
maciej-stanuch <maciejstanuch.ms@gmail.com>
2022-07-12 22:23:51 +0200

    Revert "Extract method for getting current WIP branch qualifier"
    This reverts commit 38e19a7811524a6476ea2ebf59be303d622311f4.

--- mob.go
+++ mob.go
@@ -956,7 +956,16 @@ func getMobTimerRoom(configuration Configuration) string {
 		return configuration.TimerRoom
 	}
 
-	currentWipBranchQualifier := getCurrentWipBranchQualifier(configuration)
+	currentWipBranchQualifier := configuration.WipBranchQualifier
+	if currentWipBranchQualifier == "" {
+		currentBranch := gitCurrentBranch()
+		currentBaseBranch, _ := determineBranches(currentBranch, gitBranches(), configuration)
+
+		if currentBranch.IsWipBranch(configuration) {
+			wipBranchWithoutWipPrefix := currentBranch.removeWipPrefix(configuration).Name
+			currentWipBranchQualifier = removePrefix(removePrefix(wipBranchWithoutWipPrefix, currentBaseBranch.Name), configuration.WipBranchQualifierSeparator)
+		}
+	}
 
 	if configuration.TimerRoomUseWipBranchQualifier && currentWipBranchQualifier != "" {
 		sayInfo("Using wip branch qualifier for room name")
@@ -964,20 +973,6 @@ func getMobTimerRoom(configuration Configuration) string {
 	}
 
 	return configuration.TimerRoom
-}
-
-func getCurrentWipBranchQualifier(configuration Configuration) string {
-	if configuration.WipBranchQualifier == "" {
-		currentBranch := gitCurrentBranch()
-		currentBaseBranch, _ := determineBranches(currentBranch, gitBranches(), configuration)
-
-		if currentBranch.IsWipBranch(configuration) {
-			wipBranchWithoutWipPrefix := currentBranch.removeWipPrefix(configuration).Name
-			return removePrefix(removePrefix(wipBranchWithoutWipPrefix, currentBaseBranch.Name), configuration.WipBranchQualifierSeparator)
-		}
-	}
-
-	return configuration.WipBranchQualifier
 }
 
 func startBreakTimer(timerInMinutes string, configuration Configuration) {
