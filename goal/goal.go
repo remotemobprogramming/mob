ff63cc5499314907d10cdf4b2302f6f1234ef53e
Joshua Töpfer <joshua.toepfer@innoq.com>
2024-04-26 17:12:15 +0200

    error handling in goals

--- goal/goal.go
+++ goal/goal.go
@@ -2,11 +2,13 @@ package goal
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	config "github.com/remotemobprogramming/mob/v4/configuration"
 	"github.com/remotemobprogramming/mob/v4/httpclient"
 	"github.com/remotemobprogramming/mob/v4/say"
 	"io"
+	"os"
 	"strings"
 )
 
@@ -24,24 +26,44 @@ type PutGoalRequest struct {
 }
 
 func Goal(configuration config.Configuration, parameter []string) {
-	if len(parameter) > 0 {
-		if parameter[0] == "--delete" {
-			deleteCurrentGoal(configuration)
-		} else {
-			setNewGoal(configuration, strings.Join(parameter, " "))
-		}
-	} else {
-		showGoal(configuration)
+	if err := goal(configuration, parameter); err != nil {
+		say.Error(err.Error())
+		exit(1)
 	}
 }
 
-func setNewGoal(configuration config.Configuration, goal string) {
+func goal(configuration config.Configuration, parameter []string) error {
+	if configuration.TimerRoom == "" {
+		return errors.New("No room sepcified. Set MOB_TIMER_ROOM to your timer.mob.sh room in .mob file.")
+	}
+	if len(parameter) > 0 {
+		if parameter[0] == "--delete" {
+			err := deleteCurrentGoal(configuration)
+			if err != nil {
+				return err
+			}
+		} else {
+			err := setNewGoal(configuration, strings.Join(parameter, " "))
+			if err != nil {
+				return err
+			}
+		}
+	} else {
+		err := showGoal(configuration)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func setNewGoal(configuration config.Configuration, goal string) error {
 	if err := putGoalHttp(goal, configuration); err != nil {
-		say.Error("Could not set new goal. An error happened while sending the request.")
 		say.Debug(err.Error())
-		return
+		return errors.New("Could not set new goal. An error occurred while sending the request.")
 	}
 	say.Info(fmt.Sprintf("Set new goal to \"%s\"", goal))
+	return nil
 }
 
 func putGoalHttp(goal string, configuration config.Configuration) error {
@@ -57,14 +79,14 @@ func getGoalUrl(configuration config.Configuration) string {
 	return configuration.TimerUrl + configuration.TimerRoom + "/goal"
 }
 
-func deleteCurrentGoal(configuration config.Configuration) {
+func deleteCurrentGoal(configuration config.Configuration) error {
 	err := deleteGoalHttp(configuration.TimerRoom, configuration.TimerUser, configuration.TimerUrl, configuration.TimerInsecure)
 	if err != nil {
-		say.Error("Could not delete goal. An error happened while sending the request.")
 		say.Debug(err.Error())
-		return
+		return errors.New("Could not delete goal. An error occurred while sending the request.")
 	}
 	say.Info("Current goal has been deleted!")
+	return nil
 }
 
 func deleteGoalHttp(room string, user string, timerService string, disableSslVerification bool) error {
@@ -76,36 +98,42 @@ func deleteGoalHttp(room string, user string, timerService string, disableSslVer
 	return err
 }
 
-func showGoal(configuration config.Configuration) {
+func showGoal(configuration config.Configuration) error {
 	goal, err := getGoalHttp(configuration.TimerRoom, configuration.TimerUrl, configuration.TimerInsecure)
 	if err != nil {
-		say.Error(err.Error())
-		return
+		say.Debug(err.Error())
+		return errors.New("Could not get goal. An error occurred while sending the request.")
 	}
 	if goal == "" {
 		say.Fix("No goal set. To set a goal, use", configuration.Mob("goal <your awesome goal>"))
-		return
+		return nil
 	}
 	say.Info(goal)
+	return nil
 }
 func getGoalHttp(room string, timerService string, disableSslVerification bool) (string, error) {
-	response, err := httpclient.GetHttpClient(disableSslVerification).Get(timerService + room + "/goal")
+	url := timerService + room + "/goal"
+	response, err := httpclient.GetHttpClient(disableSslVerification).Get(url)
 	if err != nil {
 		say.Debug(err.Error())
-		return "", fmt.Errorf("Could not get goal, got an error while requesting it: %w", err)
+		return "", err
 	}
-	if response.StatusCode == 204 {
-		return "", nil
+	if response.StatusCode >= 300 {
+		return "", errors.New("got an error while requesting it: " + url + " " + response.Status)
 	}
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
 		say.Debug(err.Error())
-		return "", fmt.Errorf("Could not get goal, got an error while requesting it: %w", err)
+		return "", err
 	}
 	var goalResponse GoalResponse
 	if err := json.Unmarshal(body, &goalResponse); err != nil {
 		say.Debug(err.Error())
-		return "", fmt.Errorf("Could not get goal, got an error while parsing response: %w", err)
+		return "", err
 	}
 	return goalResponse.Goal, nil
+}
+
+var exit = func(code int) {
+	os.Exit(code)
 }
--- help/help.go
+++ help/help.go
@@ -31,6 +31,9 @@ Basic Commands with Options:
     [--squash-wip]                       Squash wip commits from wip branch, maintaining manual commits
   reset
     [--branch|-b <branch-postfix>]       Set wip branch to 'mob/<base-branch>` + configuration.WipBranchQualifierSeparator + `<branch-postfix>'
+  goal 									 Gives you the current goal of your timer.mob.sh room
+	[<your-goal>]						 Sets the goal of your timer.mob.sh room
+	[--delete]							 Deletes the goal of your timer.mob.sh room
 
 Timer Commands:
   timer <minutes>           Start a <minutes> timer
