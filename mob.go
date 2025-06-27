2c5aa9862146cb8b0c93df73aeef8fcdcf52fcf6
Harry <hjwp2@cantab.net>
2025-06-27 11:34:45 +0100

    Only git fetch specific remote branch
    On a repo with a significant number of remote branches,
    fetching all remotes takes a substantial amount of time
    (literal minutes in our case, with thousands of remotes)
    
    This changes each `git fetch` call to fetch only the current branch name.

--- mob.go
+++ mob.go
@@ -408,9 +408,9 @@ func helpRequested(parameter []string) bool {
 }
 
 func clean(configuration config.Configuration) {
-	git("fetch", configuration.RemoteName, "--prune")
-
 	currentBranch := gitCurrentBranch()
+	git("fetch", configuration.RemoteName, currentBranch.Name)
+
 	localBranches := gitBranches()
 
 	if currentBranch.isOrphanWipBranch(configuration) {
@@ -549,8 +549,9 @@ func start(configuration config.Configuration) error {
 		return errors.New("cannot start; clean working tree required")
 	}
 
-	git("fetch", configuration.RemoteName, "--prune")
 	currentBranch := gitCurrentBranch()
+	git("fetch", configuration.RemoteName, currentBranch.Name)
+
 	currentBaseBranch, currentWipBranch := determineBranches(currentBranch, gitBranches(), configuration)
 
 	if !currentWipBranch.hasRemoteBranch(configuration) && configuration.StartJoin {
@@ -942,9 +943,11 @@ func done(configuration config.Configuration) {
 		return
 	}
 
-	git("fetch", configuration.RemoteName, "--prune")
+	currentBranch := gitCurrentBranch()
 
-	baseBranch, wipBranch := determineBranches(gitCurrentBranch(), gitBranches(), configuration)
+	git("fetch", configuration.RemoteName, currentBranch.Name)
+
+	baseBranch, wipBranch := determineBranches(currentBranch, gitBranches(), configuration)
 
 	if wipBranch.hasRemoteBranch(configuration) {
 		if configuration.DoneSquash == config.SquashWip {
